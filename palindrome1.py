99ae0661202e8766ce4995e46bd7c4115fa0adce
jayarajofficial6-jpg <jayarajofficial6@gmail.com>
2025-11-14 11:03:04 +0530

    my first commit

--- palindrome1.py
+++ palindrome1.py
@@ -1,14 +1,7 @@
-# palindrome.py
+text = "gadag"
 
-def is_palindrome(s):
-    s = s.replace(" ", "").lower()
-    return s == s[::-1]
-
-test_string = "madam"  # default for Jenkins
-
-if __name__ == "__main__":
-    print(f"Checking: {test_string}")
-    if is_palindrome(test_string):
-        print("It is a palindrome.")
-    else:
-        print("It is NOT a palindrome.")
+# Check palindrome
+if text == text[::-1]:
+    print(f"{text} is a palindrome")
+else:
+    print(f"{text} is NOT a palindrome")
